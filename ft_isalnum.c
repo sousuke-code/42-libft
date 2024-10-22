38a8e78147affcecfbd798da2618f79338bde8d7
sousuke-code <msousuke1@gmail.com>
2024-10-22 21:15:05 +0900

    make ft_isalnum

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 20:59:09 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/22 21:07:36 by miyatasoujo      ###   ########.fr       */
+/*   Created: 2024/10/22 21:08:12 by miyatasoujo       #+#    #+#             */
+/*   Updated: 2024/10/22 21:14:31 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "stdio.h"
 
-int ft_isdigit(char c)
+int ft_isalnum(char c)
 {
-    if (c >= '0' && c <= '9')
+    if ((c >= 'a' && c <= 'z' )|| (c >= 'A' && c <= 'Z') || (c >= '0' && c < '9'))
     {
         return 1;
     }
@@ -26,7 +26,6 @@ int ft_isdigit(char c)
 
 int main(void)
 {
-    char c = 'A';
-    printf("%d", ft_isdigit(c));
-    
+    char c = '8';
+    printf("%d", ft_isalnum(c));
 }
