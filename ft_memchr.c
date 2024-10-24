fad4aae0c914d17256fcf6259195794e38035b1d
sousuke-code <msousuke1@gmail.com>
2024-10-25 00:03:08 +0900

    make memchr  テスト少ししかしていない

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:46:56 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/24 23:28:50 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/24 23:59:43 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ void *ft_memchr(const void *s, int c, size_t n)
     size_t i;
     i = 0;
 
+    const unsigned char *p  =  (const unsigned char *)s;
+    
+
     // sをポインタ移動したものを返す。
     // 引数で受け取ったsはvoid型なのでunsigned char型に変更する必要がある。
     // また、sについては、const修飾子がついているためconst 修飾子をはずす必要がある。
@@ -24,12 +27,12 @@ void *ft_memchr(const void *s, int c, size_t n)
 
     while(i < n)
     {
-        if((unsigned char)*s == (unsigned char)c)
+        if(*p == (unsigned char)c)
         {
-            return (unsigned char )&s;
+            return (void *)p;
         }
         i++;
-        s++;
+        p++;
         
     }
     return NULL;
