09e681b77386c02cd7a60ca098471c0ba57a7807
sousuke-code <msousuke1@gmail.com>
2024-10-25 00:41:04 +0900

    make memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,13 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 21:41:23 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/22 21:53:01 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/25 00:29:06 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <stdio.h>
+#define BUF_SIZE 16
 
 void *ft_memset(void *s, int c, size_t n)
 {
@@ -21,5 +22,41 @@ void *ft_memset(void *s, int c, size_t n)
     
     // int c に関してはunsigned charに変換される
     // ｀nを0になるまでループさせ中で
+
+    unsigned char *p  = (unsigned char *)s;
+
+    while(n > 0)
+    {
+        *p = (unsigned char)c;
+        p++;
+        n--;
+    }
+    return s;
 }
 
+int main(void)
+{
+    // 変数定義
+    char buf1[BUF_SIZE];
+    char buf2[BUF_SIZE];
+
+    // 変数初期化
+    ft_memset(buf1, '\0', BUF_SIZE);
+    ft_memset(buf2, '\0', BUF_SIZE);
+
+    // 配列の内容を表示
+    printf("初期化後の配列の要素を表示。\n");
+    printf("buf1:%s\n", buf1);
+    printf("buf2:%s\n", buf2);
+
+    // メモリ領域を特定の文字で埋める
+    ft_memset(buf1, 'a', 4);  // buf1の最初の4バイトをaにする
+    ft_memset(buf1, 'b', 2);  // buf1の最初の2バイトをbにする
+    ft_memset(buf2, 'c', 3);  // buf2の最初の3バイトをcにする
+
+    // 配列の内容を表示
+    printf("配列の要素を表示。\n");
+    printf("buf1:%s\n", buf1);
+    printf("buf2:%s\n", buf2);
+	return 0;
+}
