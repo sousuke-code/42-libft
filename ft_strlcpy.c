ced79d6797ce8ec566635e33efd92bbca95eec7a
Sosuke Miyata <sosmiyat@c2r1s3.42tokyo.jp>
2024-10-25 14:07:22 +0900

    make ft_strlcpy 簡単なテスト済み

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,14 +1,45 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
+/*   By: sosmiyat <sosmiyat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 00:03:47 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/25 00:03:59 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/25 13:43:55 by sosmiyat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    // src からdistに対して、size - 1バイトを複写する。
+    size_t n;
+    n = 0;
+    while (src[n])
+    { 
+      n++;  
+    }
+    size_t i = 0; //戻り地で返すsrcの長さ
+    while (i  < size -1 && *src != '\0')
+    {
+        *dst = *src;
+        dst++;
+        src++;
+        i++;
+    }
+
+    *dst = '\0'; 
+    return n;
+}
+
+
+int main(void)
+{
+    char s[4] = {'-','-','-','-'};
+    printf("%zu", ft_strlcpy(s, "abczzzzzz", 3));
+    printf("%s",s);
+}
