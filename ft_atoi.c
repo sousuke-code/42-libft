34d318cf4c3e54baea88b4c9e4c1a1e17df3d7ed
Sosuke Miyata <sosmiyat@c2r3s1.42tokyo.jp>
2024-10-26 11:54:51 +0900

    make atoi 少し修正をおこなった

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
+/*   By: sosmiyat <sosmiyat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 18:57:48 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/26 00:54:52 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/26 11:54:29 by sosmiyat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <limits.h>
 
 
 
@@ -21,9 +22,9 @@ int ft_atoi(const char *string)
  sign = 0;
  result = 0;
 
- while(*string = '\t' || *string == '\n' || *string == '\v' || *string == '\f' || *string == '\r' || *string == '')
+ while(*string == '\t' || *string == '\n' || *string == '\v' || *string == '\f' || *string == '\r' || *string == ' ')
  {
-    str++;
+    string++;
  }   
 
  while(*string == '+' || *string == '-')
@@ -34,9 +35,8 @@ int ft_atoi(const char *string)
     string++;
  } 
 
- while((*string >= '0' && *string <= '9') && *str != '\0')
+ while((*string >= '0' && *string <= '9') && *string != '\0')
  {
-    digit = *str - '0' //次に入れる数字
 
     
     //オバーフロー、アンダーフロー対策をここで行う必要がある。
@@ -44,26 +44,22 @@ int ft_atoi(const char *string)
     //ov_mod = LONG_MAX % 10
     //numがov_divを超えた場合にオーバーフロー判定を行う。
 
-    if (sign == 1 && )
-    {
-
-    }
 
     if (sign == 1 ){
-        if (LONG_MAX / 10 < num || (LONG_MAX / 10 == num && LONG_MAX % 10 > *str)) {
+        if ( LONG_MAX / 10 < result || (LONG_MAX / 10 == result && LONG_MAX % 10 > *string)) {
             return (int)LONG_MAX;
         }
     }
 
     if (sign == -1){
-        if (LONG_MIN / 10 < num || (LONG_MIN / 10 == num && LONG_MIN % 10 < *str)) {
+        if (LONG_MIN / 10 < result || (LONG_MIN / 10 == result && LONG_MIN % 10 < *string)) {
             return (int)LONG_MIN;
         }
     }
     
-    num = 10 * result + (*str - '0');
-    str++;
+    result = 10 * result + (*string - '0');
+    string++;
  }
 
- return (int)num * sign;
+ return (int)result * sign;
 }
