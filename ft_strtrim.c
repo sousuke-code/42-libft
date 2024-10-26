b044b9bb906364698e2cc97d0a66338ea2deac7f
sousuke-code <msousuke1@gmail.com>
2024-10-26 22:12:20 +0900

    make ft_strtrim mallocチェクなど、厳しチェックまだです

---
+++ ft_strtrim.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 19:11:36 by miyatasoujo       #+#    #+#             */
+/*   Updated: 2024/10/26 22:11:32 by miyatasoujo      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int ft_strlen(char *c)
+{
+    int i;
+    i = 0;
+    while(*c) {
+        c++;
+        i++;
+    }
+    return i;
+}
+
+int pattern_match(char c, char *str)
+{
+    int i;
+    int ans;
+    ans = 0;
+    i = 0;
+    while(str[i] != '\0') {
+        if (c == str[i]) {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+    char *str;
+    int len;
+    int i;
+    i = 0;
+    len = ft_strlen((char *)s1);
+    str = malloc(len +1);
+    while(*s1) {
+        if (pattern_match(*s1, (char *)set) == 0){
+            str[i] = *s1;
+            i++;
+        }
+        s1++;
+    }
+    str[i] = '\0';
+    
+    return str;
+
+}
+
+int main(void)
+{
+
+    printf("%s", ft_strtrim("1234AAAAA2223331122","1234"));
+    
+}
