3b0a4246066eb933c7635f5ab72bf26dacadb28e
Sosuke Miyata <sosmiyat@c2r1s1.42tokyo.jp>
2024-10-27 11:29:01 +0900

    make ft_itoa 簡単なチェック済み

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,54 +3,66 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
+/*   By: sosmiyat <sosmiyat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 01:05:31 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/27 01:37:45 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/27 11:28:32 by sosmiyat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 
-int ft_strlen(char *str)
+int	count_digits(int n)
 {
-    int i;
-    while(*str) {
-        str++;
-        i++;
-    }
-    return i;
+	int	count;
+
+	count = 0;
+	if (n <= 0)
+	{
+		n = -n;
+		count++;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
 }
 
+// n = 162222
+// % =一位のくらい
+// / =1の位を除いた数
 
-char *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-    int sign;
-    int cpy;
-    char *str;
-    sign = 0;
-    if (n < 0) {
-        sign =1;
-        n = -n;
-    }
+	int		len;
+	int		sign;
+	char	*str;
 
-    cpy = n;
+	sign = 0;
+	len = count_digits(n);
+	if (n < 0)
+	{
+		n = -n;
+		str[0] = '-';
+	}
 
-    while(cpy > 0) {
-        cpy = cpy / 10;
-    }
+	str = malloc(len + 1);
+	while (n > 0)
+	{
+		str[len-1] = (n % 10) + '0';
+		n = n / 10;
+		len--;
+	}
+	return str;
+}
 
-    if (sign = 1){
-    str = malloc(cpy+2);
-    }else {
-        str = malloc(cpy + 1);
-    }
-
-    while(n > 0) {
-        *str = n % 10 + '0';  
-        str++;
-        n = n / 10;
-    }
-    
-       
+int	main(void)
+{
+	int i = 19899898;
+	char *ans;
+	ans = ft_itoa(i);
+	printf("%s\n", ans);
 }
