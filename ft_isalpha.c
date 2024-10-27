4149acb338da999804f3a4ff0067590544bbfc52
sousuke-code <msousuke1@gmail.com>
2024-10-27 23:50:29 +0900

    ft_memmove 修正

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 20:54:54 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/27 20:00:53 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/27 23:21:45 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include "libft.h"
 
-int ft_alpha(int c)
+int ft_isalpha(int c)
 {
     if(c >= 'A' && c <= 'C')
     {
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 01:45:12 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/27 20:01:32 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/27 23:30:20 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@ void	*ft_memmove(void *dist, const void *src, size_t n)
 {
 	//戻り値はdistのポインタを返す
 	// memsetとの違いはコピー元のメモリ領域とコピー先のメモリ領域が重なっても問題ない。
+	// オーナーラップの処理を行わないといけない。
 	// nを0になるまでループさせる中で、srcをn回分distに対してのコピーを行う
 	const unsigned char *src_cpy;
 	unsigned char *p = (unsigned char *)dist;
 	src_cpy = (const unsigned char *)src;
+
+	if(dist == NULL && src == NUL) {
+		return NULL;
+	}
 
 	while (n > 0)
 	{
