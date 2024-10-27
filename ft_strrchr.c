2ee370b790319aded6eec123b738527f2873b82c
sousuke-code <msousuke1@gmail.com>
2024-10-28 08:43:22 +0900

    ft_strrchar テスター通過

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 00:19:52 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/27 20:03:07 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/28 08:41:54 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ char *ft_strrchr(const char *s, int c)
 {
     int i;
     i = 0;
-    if (c == '\0')
-    {
-        return (char *)s;
-    }
 
     while(*s != '\0')
     {
