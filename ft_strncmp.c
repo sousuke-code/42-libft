5cf27bb1070695590524fdcf9b214caf45e26983
sousuke-code <msousuke1@gmail.com>
2024-10-28 10:06:48 +0900

    ft_strcmp テスター通過

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+ /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
@@ -20,9 +20,9 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 
     while((*s1 != '\0' || *s2 !='\0') && i < n)
     {
-        if (*s1 != *s2)
+        if ((unsigned char)*s1 != (unsigned char)*s2)
         {
-            return (int)(*s1 - *s2);
+            return (int)((unsigned char)*s1 - (unsigned char)*s2);
         }
         s1++;
         s2++;
