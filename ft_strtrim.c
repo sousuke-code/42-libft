ebf98bb90ede9799e534d74a6a2a4b834f1cf21c
sousuke-code <msousuke1@gmail.com>
2024-10-30 21:36:14 +0900

    ft_strtrim テスター通過

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 19:11:36 by miyatasoujo       #+#    #+#             */
-/*   Updated: 2024/10/27 20:05:07 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/30 21:34:46 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,28 @@ int pattern_match(char c, char *str)
 
 char *ft_strtrim(char const *s1, char const *set)
 {
+    if (s1 == NULL || set == NULL)
+     return NULL;
+    
     char *str;
-    int len;
-    int i;
-    i = 0;
+    size_t len;
+    size_t start;
+    size_t end;
+    start = 0;
     len = ft_strlen((char *)s1);
-    str = malloc(len +1);
-    while(*s1) {
-        if (pattern_match(*s1, (char *)set) == 0){
-            str[i] = *s1;
-            i++;
-        }
-        s1++;
-    }
-    str[i] = '\0';
+    end = len;
+
+    while(s1[start] != '\0' && pattern_match(s1[start],(char *)set))
+      start++;
+
+    while(end > start && pattern_match(s1[end-1], (char *)set))
+      end--;
+
+    str = malloc(end - start +1);
+    if (str == NULL)
+      return NULL;
+
+    ft_strlcpy(str,&s1[start], end - start + 1);
     
     return str;
 
