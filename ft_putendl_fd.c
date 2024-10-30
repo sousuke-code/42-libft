7d38b4232b719dd06027bc0e16b1de43c9de8769
sousuke-code <msousuke1@gmail.com>
2024-10-31 00:57:59 +0900

    fd 関数のエラー処理追加

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 13:58:10 by sosmiyat          #+#    #+#             */
-/*   Updated: 2024/10/27 20:07:19 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/31 00:55:22 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 
 void ft_putendl_fd(char *s, int fd)
 {
+    if (s == NULL)
+      return ;
     write(fd,s,ft_strlen(s));
     write(fd, "\n", 1);
 }
--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 11:34:23 by sosmiyat          #+#    #+#             */
-/*   Updated: 2024/10/27 20:02:00 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/31 00:56:51 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
     // 第一引数　反復処理の対象となる引数
     // 各文字に適用する関数
+    if (s == NULL)
+      return ;
     unsigned int i;
     i = 0;
     while(*s) {
