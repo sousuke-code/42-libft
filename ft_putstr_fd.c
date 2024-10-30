35d14022a34c4b41c7d45227c13bf923148406a8
sousuke-code <msousuke1@gmail.com>
2024-10-31 00:54:58 +0900

    ft_putstr_fd 修正しました

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 13:53:37 by sosmiyat          #+#    #+#             */
-/*   Updated: 2024/10/27 20:07:03 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/31 00:53:54 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 
 void ft_putstr_fd(char *s, int fd)
 {
+   if (s == NULL )
+     return;
    write(fd,s,ft_strlen(s));
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: miyatasoujo <miyatasoujo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 12:05:04 by sosmiyat          #+#    #+#             */
-/*   Updated: 2024/10/27 20:11:25 by miyatasoujo      ###   ########.fr       */
+/*   Updated: 2024/10/31 00:50:44 by miyatasoujo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,19 @@ char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
     int len;
     unsigned int i;
     i = 0;
+    if (s == NULL)
+      return NULL;
 
     len = ft_strlen((char *)s);
     str = malloc(len + 1);
+    if (str ==NULL)
+       return NULL;
     while(s[i] != '\0') {
         str[i] = f(i,s[i]);
         i++;
     }
-    str[len+1] = '\0';
+    str[len] = '\0';
     return str;
 }
+
 
